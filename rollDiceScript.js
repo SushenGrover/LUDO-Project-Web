63557c50022b1452e9539ba259fd67c8fb37c9ee
SushenGrover <sushen.grover2023@vitstudent.ac.in>
2025-04-11 22:37:12 +0530

    Updated on 11 april

--- rollDiceScript.js
+++ rollDiceScript.js
@@ -1,47 +1,3 @@
-let redPawns = document.querySelectorAll(".red-pawn");
-// console.log(redPawns);
-let yellowPawns = document.querySelectorAll(".yellow-pawn");
-// console.log(yellowPawns);
-let bluePawns = document.querySelectorAll(".blue-pawn");
-// console.log(bluePawns);
-let greenPawns = document.querySelectorAll(".green-pawn");
-// console.log(greenPawns);
-
-// fetching cells in the form of path arrays
-function fetchCells(color) {
-  let path = [];
-  for (let i = 1; i <= 56; i++) {
-    let name = "." + color + i;
-    // console.log(name);
-    let cell = document.querySelector(name);
-    path.push(cell);
-    // cell.textContent=i;
-    // cell.style.backgroundColor="green";
-  }
-  return path;
-}
-function fetchStartCells(color) {
-  let arr = document.querySelectorAll("." + color + "-start-cell");
-  // console.log(arr);
-  return arr;
-}
-
-
-let greenPath = fetchCells("g");
-let redPath = fetchCells("r");
-let bluePath = fetchCells("b");
-let yellowPath = fetchCells("y");
-
-let greenStartCells = fetchStartCells("green");
-let redStartCells = fetchStartCells("red");
-let yellowStartCells = fetchStartCells("yellow");
-let blueStartCells = fetchStartCells("blue");
-
-let i = 0;
-//movement functions
-//move after 800ms after dice button is pressed bcoz it takes 700 ms for dice rolling
-
-
 dicePositions=new Array(6);
 container=document.querySelector(".dice-container");
 displayNum=document.querySelector(".display-turn-num");
@@ -56,14 +12,6 @@ dicePositions[4].innerHTML='<div style="font-size: 0.7rem;width: 4rem;height: 4r
 dicePositions[5].innerHTML='<div style="font-size: 0.7rem;width: 4rem;height: 4rem;display: grid;align-items: center;justify-items: center;color: red;grid-template-columns: 1fr 1fr;grid-template-rows: 1fr 1fr 1fr;" class="dice-position-6 dice-position"><div><i class="fa-solid fa-circle"></i></div><div><i class="fa-solid fa-circle"></i></div><div><i class="fa-solid fa-circle"></i></div><div><i class="fa-solid fa-circle"></i></div><div><i class="fa-solid fa-circle"></i></div><div><i class="fa-solid fa-circle"></i></div></div>';
 container.appendChild(dicePositions[0]);
 
-let turnSpecifierText=document.querySelector("#turn-specifier-div");
-let currentPlayer="red";
-let diceValue=0;
-let gameState="waitingForRoll";
-
-function startTurn(){
-  
-}
 
 function rollDice(event) {
     event.preventDefault();
@@ -77,44 +25,17 @@ function rollDice(event) {
             setTimeout(showNextDice, 100);
         } else {
             showFinalDice();
-            startTurn();
         }
     }
     function showFinalDice() {
         container.innerHTML = '';
-        // let num = Math.floor(Math.random() * 6) + 1;
-        let num=6;
-        diceValue=num;
+        let num = Math.floor(Math.random() * 6) + 1;
+        // let num=6;
         container.appendChild(dicePositions[num - 1]);
         displayNum.innerHTML=num;
+
     }
     showNextDice();
 }
 
-
-
-function transferPawn(oldParent, child, newParent) {
-  const oldRect = oldParent.getBoundingClientRect();
-  const newRect = newParent.getBoundingClientRect();
-
-  const deltaX = oldRect.left - newRect.left;
-  const deltaY = oldRect.top - newRect.top;
-
-  child.style.transition = 'none';
-  child.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
-
-  newParent.appendChild(child);
-
-  requestAnimationFrame(() => {
-    child.style.transition = 'transform 1s ease-in-out';
-    child.style.transform = 'translate(0, 0)';
-  });
-
-  child.addEventListener('transitionend', () => {
-    child.style.transition = '';
-    child.style.transform = '';
-  }, { once: true });
-}
-
-let playButton=document.querySelector(".play-button");
-playButton.addEventListener('click', rollDice);
+document.querySelector(".play-button").addEventListener('click', rollDice);
